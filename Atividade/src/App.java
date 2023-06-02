2d745aee308dd0a926ed746991837fdfa2ec5080
lucassaureliano <lucassaureliano@gmail.com>
2023-06-02 18:19:47 -0300

    Criação do arquivo

---
+++ Atividade/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public enum EstadoCivil {
+        SOLTEIRO,
+        CASADO,
+        SEPARADO,
+        DIVORCIADO,
+        VIUVO
+}
+
+}
